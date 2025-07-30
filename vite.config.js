9f60b0ee219e266abccf51da0d308c9b6bc0bc80
Talha Ghauri <talhaghauridev@gmail.com>
2025-07-30 12:33:17 +0500

    upgrade vitejs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+   base: "/",
+   plugins: [
+      react(),
+      VitePWA({
+         registerType: "autoUpdate",
+         workbox: {
+            globPatterns: ["**/*.{png,jpg,jpeg,svg,gif,webp,ico}"],
+
+            runtimeCaching: [
+               {
+                  urlPattern: ({ request }) => request.destination === "image",
+                  handler: "CacheFirst",
+                  options: {
+                     cacheName: "image-cache",
+                     expiration: {
+                        maxEntries: 100,
+                        maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
+                     },
+                     cacheableResponse: { statuses: [0, 200] }
+                  }
+               }
+            ]
+         }
+      })
+   ],
+   resolve: {
+      alias: { "@": path.resolve(__dirname, "./src") }
+   },
+   build: {
+      outDir: "dist",
+      terserOptions: {
+         compress: {
+            dead_code: true,
+            drop_console: true,
+            pure_funcs: ["console.log"]
+         }
+      }
+   }
+});
